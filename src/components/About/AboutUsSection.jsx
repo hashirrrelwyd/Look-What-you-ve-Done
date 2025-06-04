b1e2d4cbd1ab44fb1f54d8fa41894693667cfac7
MUHAMMED HASHIR <muhammedhashir298@gmail.com>
2025-06-04 19:13:06 +0530

    about page updation

--- src/components/About/AboutUsSection.jsx
+++ src/components/About/AboutUsSection.jsx
@@ -1,21 +1,22 @@
 import { useCursor } from "../../context/CursorContext";
+import "./css/about.css"
 
 export default function AboutUsSection() {
   const { setHoverType } = useCursor();
   return (
     <div
-      className="flex px-6 md:px-10 h-screen bg-white py-16 gap-12"
+      className="about-section flex px-6 md:px-10 bg-white py-16 gap-12"
       onMouseEnter={() => setHoverType("black")}
       onMouseLeave={() => setHoverType("default")}
     >
-      <div className="w-2/4">
+      <div className="hidden md:block w-2/4">
         <img
           src="/images/about.jpg"
           alt=""
-          className=" h-[600px] w-[500px] object-cover rounded-md"
+          className="about-img h-[600px] w-[800px] object-cover rounded-md"
         />
       </div>
-      <div className="flex flex-col justify-between w-2/4">
+      <div className="hidden md:flex flex-col justify-between w-2/4">
         <div className="flex">
           <h2
             className="text-black text-xs lg:text-lg"
@@ -26,7 +27,7 @@ export default function AboutUsSection() {
           </h2>
         </div>
         <p
-          className="text-xl text-gray-400"
+          className="text-sm lg:text-xl text-gray-400"
           onMouseEnter={() => setHoverType("big-black")}
           onMouseLeave={() => setHoverType("black")}
         >
@@ -37,6 +38,38 @@ export default function AboutUsSection() {
           ipsum dolor sit amet, consectetur adipiscing{" "}
         </p>
       </div>
+
+      {/* Mobile Version */}
+      <div className="flex md:hidden flex-col gap-4 justify-between">
+        <div className="flex">
+          <h2
+            className="text-black text-xs lg:text-lg"
+            onMouseEnter={() => setHoverType("big-black")}
+            onMouseLeave={() => setHoverType("black")}
+          >
+            <span className="text-lwyd-yellow">/ </span>About Us
+          </h2>
+        </div>
+        <div className="md:hidden">
+        <img
+          src="/images/about.jpg"
+          alt=""
+          className="about-img-mobile h-[300px] w-full object-cover rounded-md"
+        />
+      </div>
+        <p
+          className="text-sm lg:text-xl text-gray-400"
+          onMouseEnter={() => setHoverType("big-black")}
+          onMouseLeave={() => setHoverType("black")}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,{" "}
+          <span className="text-lwyd-yellow">sed do Lorem </span>
+          ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit,{" "}
+          <span className="text-lwyd-yellow">sed do Lorem </span> ipsumLorem
+          ipsum dolor sit amet, consectetur adipiscing{" "}
+        </p>
+      </div>
+
     </div>
   );
 }
--- src/components/About/LwydTeam.jsx
+++ src/components/About/LwydTeam.jsx
@@ -2,163 +2,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useCursor } from "../../context/CursorContext";
 // import "./css/lwydteam.css"
-
-const teamData = [
-  {
-    title: "Sumath Karnad",
-    subtitle: "Give me an update?",
-    category: "Leadership",
-    image: "/team-imgs/Sumath1.svg",
-  },
-  {
-    title: "Malavika Pai",
-    subtitle: "Listen, Listen, Listen",
-    category: "Leadership",
-    image: "/team-imgs/Malavika1.svg",
-  },
-  {
-    title: "Raghav Rithvik",
-    subtitle: "I'm with you, but...",
-    category: "Leadership",
-    image: "/team-imgs/Raghav1.svg",
-  },
-  {
-    title: "Sharath Menon",
-    subtitle: "Basically...",
-    category: "Lead",
-    image: "/team-imgs/Sharath1.svg",
-  },
-  {
-    title: "Vanmayi Shetty",
-    subtitle: "HAHAHA 😂",
-    category: "Lead",
-    image: "/team-imgs/Vanmayi1.svg",
-  },
-  {
-    title: "Sijin T G",
-    subtitle: "Today not possible",
-    category: "Lead",
-    image: "/team-imgs/Sijin1.svg",
-  },
-  {
-    title: "Vishnu",
-    subtitle: "Question...",
-    category: "Lead",
-    image: "/team-imgs/VISHNU 2.png",
-  },
-  {
-    title: "Yokeshwar Elangovan",
-    subtitle: "Where's the calendar bro?",
-    category: "Lead",
-    image: "/team-imgs/yoke.jpeg",
-  },
-  {
-    title: "Sanya Afreen",
-    subtitle: "Guys, we can do this!",
-    category: "Lead",
-    image: "/team-imgs/Sanya1.svg",
-  },
-  {
-    title: "Sujay H",
-    subtitle: "Bedsheet Not Spreadshee",
-    category: "Designer",
-    image: "/team-imgs/Sujay1.svg",
-  },
-  {
-    title: "Jishin A",
-    subtitle: "May the fonts be with you",
-    category: "Designer",
-    image: "/team-imgs/Jishin-1.png",
-  },
-  {
-    title: "Abdul Manaf K",
-    subtitle: "Pssst....I see dead pixels",
-    category: "Designer",
-    image: "/team-imgs/Manaf-1.png",
-  },
-  {
-    title: "Nihal Daniel",
-    subtitle: "Guys, Drink Water!",
-    category: "Designer",
-    image: "/team-imgs/Nihaal1.svg",
-  },
-  {
-    title: "Advaith MP",
-    subtitle: "The city needs me",
-    category: "Designer",
-    image: "/team-imgs/Advaith-1.jpg",
-  },
-  {
-    title: "Jolan Dsouza",
-    subtitle: "That's what she said.",
-    category: "Designer",
-    image: "/team-imgs/Jolan-1.png",
-  },
-  {
-    title: "Saumya Kharkar",
-    subtitle: "I cared for eight seconds, then I got distracted.",
-    category: "Designer",
-    image: "/team-imgs/Soumya-1.png",
-  },
-  {
-    title: "Ruthik A",
-    subtitle: "It's rendering maga",
-    category: "Designer",
-    image: "/team-imgs/Ruthik-1.png",
-  },
-  ,
-  {
-    title: "Sagar",
-    subtitle: "Lights, Camera, Caffiene!",
-    category: "Designer",
-    image: "/team-imgs/Sagar-1.png",
-  },
-  {
-    title: "Samuel S",
-    subtitle: "Game is game",
-    category: "Designer",
-    image: "/team-imgs/Samuel-1.png",
-  },
-  ,
-  {
-    title: "Muhammed Hashir",
-    subtitle: "Let's go wild!",
-    category: "Developer",
-    image: "/team-imgs/Hashir-1.png",
-  },
-//   {
-//     title: "Athin Samuel",
-//     subtitle: ".......",
-//     category: "Designer",
-//     image: "/team-imgs/Athin.JPG",
-//   },
-//   {
-//     title: "Athira",
-//     subtitle: "A Designing force to be reckoned with!",
-//     category: "Designer",
-//     image: "/team-imgs/Athira.JPG",
-//   },
-//   {
-//     title: "Greeshma",
-//     subtitle: "Savage mode activated",
-//     category: "Designer",
-//     image: "/team-imgs/Greeshma.JPG",
-//   },
-//   {
-//     title: "Maria Gargory Christopher",
-//     subtitle: "Deadline? I thought you said wine",
-//     category: "Designer",
-//     image: "/team-imgs/Chris.JPG",
-//   },
-  {
-    title: "Manoj",
-    subtitle: "Força Barça",
-    category: "AM",
-    image: "/team-imgs/Manoj-1.png",
-  },
-  
-  
-];
+import { teamData } from "../../data/teamData";
 
 export default function LwydTeam() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -252,9 +96,9 @@ export default function LwydTeam() {
           >
             <button
               onClick={() => {
-                setCurrentCategory("all")
+                setCurrentCategory("all");
                 setCurrentIndex(0);
-              } }
+              }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
                 currentCategory === "all"
                   ? "bg-yellow-400"
@@ -270,7 +114,7 @@ export default function LwydTeam() {
           >
             <button
               onClick={() => {
-                setCurrentCategory("Leadership")
+                setCurrentCategory("Leadership");
                 setCurrentIndex(0);
               }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
@@ -287,7 +131,8 @@ export default function LwydTeam() {
             } text-[#7D7D7D] items-center gap-2`}
           >
             <button
-              onClick={() => {setCurrentCategory("Lead")
+              onClick={() => {
+                setCurrentCategory("Lead");
                 setCurrentIndex(0);
               }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
@@ -304,7 +149,8 @@ export default function LwydTeam() {
             } text-[#7D7D7D] items-center gap-2`}
           >
             <button
-              onClick={() => {setCurrentCategory("Designer")
+              onClick={() => {
+                setCurrentCategory("Designer");
                 setCurrentIndex(0);
               }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
@@ -322,7 +168,8 @@ export default function LwydTeam() {
             } text-[#7D7D7D] items-center gap-2`}
           >
             <button
-              onClick={() => {setCurrentCategory("AM")
+              onClick={() => {
+                setCurrentCategory("AM");
                 setCurrentIndex(0);
               }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
@@ -339,7 +186,8 @@ export default function LwydTeam() {
             } text-[#7D7D7D] items-center gap-2`}
           >
             <button
-              onClick={() => {setCurrentCategory("Developer")
+              onClick={() => {
+                setCurrentCategory("Developer");
                 setCurrentIndex(0);
               }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
@@ -356,7 +204,8 @@ export default function LwydTeam() {
             } text-[#7D7D7D] items-center gap-2`}
           >
             <button
-              onClick={() => {setCurrentCategory("Friyay")
+              onClick={() => {
+                setCurrentCategory("Friyay");
                 setCurrentIndex(0);
               }}
               className={`text-lg px-2 py-2 rounded-full cursor-none ${
@@ -383,27 +232,26 @@ export default function LwydTeam() {
             HR & Finance
           </div>
         </div>
-
-        {/* Thumbnails Preview */}
-        <div className="flex gap-4">
-          {filteredTeam.map((member, index) => (
-            <div
-              key={index}
-              className={`relative flex flex-col items-center ${
-                index === currentIndex ? "border-1 border-white rounded-lg" : ""
+      </div>
+      {/* Thumbnails Preview */}
+      <div className="flex gap-4 w-full overflow-x-auto overflow-y-hidden whitespace-nowrap" >
+        {filteredTeam.map((member, index) => (
+          <div
+            key={index}
+            className={`relative flex flex-col items-center ${
+              index === currentIndex ? "border-1 border-white rounded-lg" : ""
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          >
+            <img
+              src={member.image}
+              alt={member.title}
+              className={`transition-all duration-700 rounded-md object-cover w-15 h-20 ${
+                index === currentIndex ? "scale-80" : ""
               }`}
-              onClick={() => setCurrentIndex(index)}
-            >
-              <img
-                src={member.image}
-                alt={member.title}
-                className={`transition-all duration-700 rounded-md object-cover w-15 h-20 ${
-                  index === currentIndex ? "scale-80" : ""
-                }`}
-              />
-            </div>
-          ))}
-        </div>
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
